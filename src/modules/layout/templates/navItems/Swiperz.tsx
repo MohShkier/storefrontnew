897e0585432c537b6d4b94bc5b4727d6c4d18553
ZEUS . <zeus@ze0s.com>
2024-04-26 00:28:36 +0300

    Add untracked images

--- src/modules/layout/templates/navItems/Swiperz.tsx
+++ src/modules/layout/templates/navItems/Swiperz.tsx
@@ -13,7 +13,13 @@ import "./styles.css"
 import   { Navigation, Autoplay, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import Image from "next/image"
 import myImage from "../../../../app/swiperImage.jpg"
-
+import swiper2 from "../../../../app/swiper8.png"
+import swiper3 from "../../../../app/swiper2.png"
+import swiper4 from "../../../../app/swiper3.png"
+import swiper5 from "../../../../app/swiper4.png"
+import swiper6 from "../../../../app/swiper5.jpg"
+import swiper7 from "../../../../app/swiper6.jpeg"
+import swiper8 from "../../../../app/swiper7.png"
 SwiperCore.use([Navigation, Autoplay, Pagination, Scrollbar, A11y]);
 
 interface SwiperProps {
@@ -24,7 +30,7 @@ const Swiperz: React.FC<SwiperProps> = ({ classAdded }) => {
     return (
         <>
         <Swiper
-            className={classAdded + "Sswiper  mt-10  small:max-lg:!h-[10rem] 2xsmall:max-small:!w-[30rem]"}
+            className={classAdded + "Sswiper  mt-10 rounded-3xl small:max-lg:!h-[10rem] 2xsmall:max-small:!w-[30rem]"}
             spaceBetween={100}
             autoplay={{
 
@@ -33,8 +39,9 @@ const Swiperz: React.FC<SwiperProps> = ({ classAdded }) => {
             }}
             slidesPerView={1}
             navigation
-            pagination={{ clickable: true }}
-            scrollbar={{ draggable: true }}
+            pagination={{          
+              el: '.swiper-pagination',
+            clickable: true }}
         >
 <SwiperSlide className='SwiperSlider'>
         <Image
@@ -46,38 +53,87 @@ const Swiperz: React.FC<SwiperProps> = ({ classAdded }) => {
           loader={({ src }) => src}
         />
       </SwiperSlide>
+   
       <SwiperSlide className='SwiperSlider'>
         <Image
           alt=""
           className='SwiperSliderImg'
-          src={myImage}
-           width={500}
-          height={500}
-          loader={({ src }) => src}
-        />
-      </SwiperSlide>
-      <SwiperSlide className='SwiperSlider'>
-        <Image
-          alt=""
-          className='SwiperSliderImg'
-          src={myImage}
-                    width={500}
-          height={500}
-          loader={({ src }) => src}
-        />
-      </SwiperSlide>
-      <SwiperSlide className='SwiperSlider'>
-        <Image
-          alt=""
-          className='SwiperSliderImg'
-          src={myImage}
-                    width={500}
+          src={swiper2}
+          width={500}
           height={500}
           loader={({ src }) => src}
         />
       </SwiperSlide>
 
-      
+      <SwiperSlide className='SwiperSlider'>
+        <Image
+          alt=""
+          className='SwiperSliderImg'
+          src={swiper3}
+          width={500}
+          height={500}
+          loader={({ src }) => src}
+        />
+      </SwiperSlide>
+
+      <SwiperSlide className='SwiperSlider'>
+        <Image
+          alt=""
+          className='SwiperSliderImg'
+          src={swiper4}
+          width={500}
+          height={500}
+          loader={({ src }) => src}
+        />
+      </SwiperSlide>
+
+      <SwiperSlide className='SwiperSlider'>
+        <Image
+          alt=""
+          className='SwiperSliderImg'
+          src={swiper5}
+          width={500}
+          height={500}
+          loader={({ src }) => src}
+        />
+      </SwiperSlide>
+
+
+      <SwiperSlide className='SwiperSlider'>
+        <Image
+          alt=""
+          className='SwiperSliderImg'
+          src={swiper6}
+          width={500}
+          height={500}
+          loader={({ src }) => src}
+        />
+      </SwiperSlide>
+
+      <SwiperSlide className='SwiperSlider'>
+        <Image
+          alt=""
+          className='SwiperSliderImg'
+          src={swiper7}
+          width={500}
+          height={500}
+          loader={({ src }) => src}
+        />
+      </SwiperSlide>
+
+      <SwiperSlide className='SwiperSlider'>
+        <Image
+          alt=""
+          className='SwiperSliderImg'
+          src={swiper8}
+          width={500}
+          height={500}
+          loader={({ src }) => src}
+        />
+      </SwiperSlide>
+
+      <div className="swiper-pagination"></div>
+
         </Swiper>
 
 
