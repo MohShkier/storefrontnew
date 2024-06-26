46918366f5957e6ee2e55f98d2cb960c356f0b0f
Mohammed Sh <65793511+DiscordZeus@users.noreply.github.com>
2024-06-26 23:57:09 +0300

    Update search-client.ts

--- src/lib/search-client.ts
+++ src/lib/search-client.ts
@@ -1,11 +1,10 @@
-import { instantMeiliSearch } from "@meilisearch/instant-meilisearch"
+import algoliasearch from "algoliasearch/lite"
 
-const endpoint =
-  process.env.NEXT_PUBLIC_SEARCH_ENDPOINT || "http://127.0.0.1:7700"
+const appId = process.env.NEXT_PUBLIC_SEARCH_APP_ID || ""
 
-const apiKey = process.env.NEXT_PUBLIC_SEARCH_API_KEY || "test_key"
+const apiKey = process.env.NEXT_PUBLIC_SEARCH_API_KEY || ""
 
-export const searchClient = instantMeiliSearch(endpoint, apiKey)
+export const searchClient = algoliasearch(appId, apiKey)
 
 export const SEARCH_INDEX_NAME =
   process.env.NEXT_PUBLIC_INDEX_NAME || "products"
