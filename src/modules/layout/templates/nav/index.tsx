08d329ff42ee11c45d6456c20538939cd8037fc6
Mohammed Sh <65793511+DiscordZeus@users.noreply.github.com>
2024-06-28 03:30:14 +0300

    Update index.tsx

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -1,15 +1,76 @@
 
 import { Suspense } from "react"
-import { listRegions } from "@lib/data"
 import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import CartButton from "@modules/layout/components/cart-button"
 import SideMenu from "@modules/layout/components/side-menu"
 import NavItems from "@modules/layout/templates/navItems";
+import { Product } from "@medusajs/medusa"
+import { getCategoriesList, getCollectionsList, getProductsList, getRegion, } from "@lib/data"
+import { ProductCollectionWithPreviews, ProductPreviewType  } from "types/global"
+import { cache } from "react"
+
+
+  const getCollectionsWithProducts = cache(
+    async (
+      countryCode: string
+    ): Promise<ProductCollectionWithPreviews[] | null> => {
+      const { collections } = await getCollectionsList(0, 8)
+  
+      if (!collections) {
+        return null
+      }
+  
+  
+      const collectionIds = collections.map((collection) => collection.id)
+  
+      await Promise.all(
+        collectionIds.map((id) =>
+          getProductsList({
+            queryParams: { collection_id: [id] },
+            countryCode,
+          })
+        )
+      ).then((responses) =>
+        responses.forEach(({ response, queryParams }) => {
+          let collection
+  
+          if (collections) {
+            collection = collections.find(
+              (collection) => collection.id === queryParams?.collection_id?.[0]
+            )
+          }
+  
+          if (!collection) {
+            return
+          }
+  
+          collection.products = response.products as unknown as Product[]
+        })
+      )
+  
+      return collections as unknown as ProductCollectionWithPreviews[]
+    }
+  )
+
+
+
+
+
+
+
 export default async function Nav() {
-  const regions = await listRegions().then((regions) => regions)
+  const collections = await getCollectionsWithProducts("jo")
+  const region = await getRegion("jo")
+  const categories = await getCategoriesList
+  const { product_categories } = await getCategoriesList()
+  if (!collections || !region) {
+    return null
+  }
+  const filteredCollections = collections
 
   return (
-      <>
+    <>
+       <>
         <div className="sticky top-0 inset-x-0 z-50 group test">
           <nav className="bg-white shadow-lg border-b-4 border-gray-300 h-[7rem]">
             <div
@@ -68,9 +129,9 @@ export default async function Nav() {
                     <CartButton/>
                   </Suspense>
                 </div>
-                <div className="lg:hidden flex-1 basis-0 h-full  items-center">
+                <div className=" flex-1 basis-0 h-full  items-center">
                   <div className="h-full">
-                    <SideMenu regions={regions}/>
+                  <SideMenu itemsArray={collections} category={product_categories}/>
                   </div>
                 </div>
               </div>
@@ -82,8 +143,8 @@ export default async function Nav() {
           </nav>
         </div>
 
-        <NavItems/>
 
       </>
-  )
-}
+     
+    </>
+  )}
