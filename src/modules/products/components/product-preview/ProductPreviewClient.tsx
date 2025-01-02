20ef0e6523ee5f37756f253994c4369e2170751b
Mohammed Sh <65793511+MohShkier@users.noreply.github.com>
2025-01-03 01:57:35 +0300

    Update ProductPreviewClient.tsx

--- src/modules/products/components/product-preview/ProductPreviewClient.tsx
+++ src/modules/products/components/product-preview/ProductPreviewClient.tsx
@@ -17,11 +17,18 @@ const ProductPreviewClient: React.FC<ProductPreviewClientProps> = ({ product, re
   const closeModal = () => setIsModalOpen(false)
 
   return (
-    <div>
+    <div className="">
       {/* Button to open the modal */}
-      <button onClick={openModal}>Show Product Actions</button>
 
       {/* Modal Component */}
+      <div className="flex justify-center ">
+        <div  onClick={openModal} className=" hover:cursor-pointer  bg-red-500 p-1 rounded-b-lg text-white text-center w-4/5 flex gap-1 justify-center ">
+          <IoMdCart className="text-2xl  text-white hover:cursor-pointer"  />
+              <p className="text-xs md:text-lg">اضف الى السلة</p>
+
+        </div>
+        </div>
+     
       <Modal isOpen={isModalOpen} onClose={closeModal} product={product} region={region} />
     </div>
   )
