709c2059db86612b72a55144b89040e7926328bc
Mohammed Sh <65793511+MohShkier@users.noreply.github.com>
2025-01-03 19:10:29 +0300

    Update ProductPreviewClient.tsx

--- src/modules/products/components/product-preview/ProductPreviewClient.tsx
+++ src/modules/products/components/product-preview/ProductPreviewClient.tsx
@@ -1,11 +1,9 @@
-// ProductPreviewClient.tsx (Client-side)
-"use client" // Mark this as a client component
-
+"use client"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import React, { useState } from "react"
 import ProductActions from "@modules/products/components/product-actions"
 import Modal from "./Modal"
 import { IoMdCart } from "react-icons/io"
-
 interface ProductPreviewClientProps {
   product: any
   region: any
@@ -16,7 +14,6 @@ const ProductPreviewClient: React.FC<ProductPreviewClientProps> = ({ product, re
 
   const openModal = () => setIsModalOpen(true)
   const closeModal = () => setIsModalOpen(false)
-
   return (
     <div className="">
       {/* Button to open the modal */}
@@ -26,7 +23,6 @@ const ProductPreviewClient: React.FC<ProductPreviewClientProps> = ({ product, re
         <div  onClick={openModal} className=" hover:cursor-pointer  bg-red-500 p-1 rounded-b-lg text-white text-center w-4/5 flex gap-1 justify-center ">
           <IoMdCart className="text-2xl  text-white hover:cursor-pointer"  />
               <p className="text-xs md:text-lg">اضف الى السلة</p>
-
         </div>
         </div>
      
