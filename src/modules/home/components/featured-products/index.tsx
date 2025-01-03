88d0c676c17f37e934fa2fa1fcc30ed5579fdaa8
Mohammed Sh <65793511+MohShkier@users.noreply.github.com>
2025-01-03 19:09:40 +0300

    Update index.tsx

--- src/modules/home/components/featured-products/index.tsx
+++ src/modules/home/components/featured-products/index.tsx
@@ -1,18 +1,43 @@
-import { Region } from "@medusajs/medusa"
+import { Region } from "@medusajs/medusa";
 
-import ProductRail from "@modules/home/components/featured-products/product-rail"
-import { ProductCollectionWithPreviews } from "types/global"
+import ProductRail from "@modules/home/components/featured-products/product-rail";
+import Swiperz2 from "@modules/layout/templates/nav/Swiperz2";
+import { retrievePricedProductById } from "@lib/data";
+import { ProductCollectionWithPreviews } from "types/global";
 
 export default async function FeaturedProducts({
   collections,
   region,
 }: {
-  collections: ProductCollectionWithPreviews[]
-  region: Region
+  collections: ProductCollectionWithPreviews[];
+  region: Region;
 }) {
-  return collections.map((collection) => (
-    <li key={collection.id}>
-      <ProductRail collection={collection} region={region} />
-    </li>
-  ))
+  // Collect all products from collections
+  const pricedProducts: any[] = [];
+  collections.forEach((collection) => {
+    pricedProducts.push(...(collection.products || []));
+  });
+
+  // Map product IDs to priced products
+  const pricedProductsArray = await Promise.all(
+    pricedProducts.map(async (product) => {
+      const pricedProduct = await retrievePricedProductById({
+        id: product.id,
+        regionId: region.id,
+      });
+      return { ...product, pricedDetails: pricedProduct };
+    })
+  );
+
+
+  return (
+    <ul className="flex flex-col-reverse">
+      {collections.map((collection) => (
+        <li key={collection.id}>
+          <ProductRail collection={collection} region={region} />
+        </li>
+      ))}
+      <Swiperz2 classAdded="" itemsArray={pricedProductsArray} region={region} />
+    </ul>
+  );
 }
