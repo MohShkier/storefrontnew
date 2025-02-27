4d236ed9e6b4058e12a1623b0e473f988901b6c9
Mohammed Sh <65793511+MohShkier@users.noreply.github.com>
2025-02-27 17:25:30 +0300

    Update page.tsx

--- src/app/[countryCode]/(main)/page.tsx
+++ src/app/[countryCode]/(main)/page.tsx
@@ -12,7 +12,7 @@ import Swiperz from "@modules/layout/templates/navItems/Swiperz"
 import Swiperz2 from "@modules/layout/templates/nav/Swiperz2"
 import Image from 'next/image';
 import Head from 'next/head';
-import Logo from "../../mainlogo.png";
+
 
 export const metadata: Metadata = {
   title: "المحترف موبايل",
@@ -85,15 +85,15 @@ export default async function Home({
         {/* Open Graph Meta Tags */}
         <meta property="og:title" content={"المحترف موبايل"} />
         <meta property="og:description" content={"المحترف موبايل ، اكبر معرض في شرق عمان للأجهزة الخلوية واكسسواراتها "} />
-        <meta property="og:image" content={Logo} />
-        <meta property="og:url" content={`https://mohtaref.pro/jo/} />
+        <meta property="og:image" content={"../../mainlogo.png"} />
+        <meta property="og:url" content={`https://mohtaref.pro/jo/`} />
         <meta property="og:type" content="product" />
 
         {/* Twitter Card Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={"المحترف موبايل"} />
         <meta name="twitter:description" content={"المحترف موبايل ، اكبر معرض في شرق عمان للأجهزة الخلوية واكسسواراتها "} />
-        <meta name="twitter:image" content={Logo} />
+        <meta name="twitter:image" content={"../../mainlogo.png"} />
       </Head>
                 <Swiperz classAdded=""/>
       <Hero />
